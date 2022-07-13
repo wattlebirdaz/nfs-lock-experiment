2d5a24c9099cb1bbaa1eb319bc3c4492d43301c5
wattlebirdaz_optiplex <wattlebirdaz@gmail.com>
2022-07-13 12:38:24 +0900

    get lock for multiple nodes test

---
+++ get_lock.py
@@ -0,0 +1,14 @@
+from linklock1 import LinkLock1
+from linklock2 import LinkLock2
+from openlock import OpenLock
+
+def get_lock(config, dir):
+    if config == "linklock1":
+        return LinkLock1(dir + "/./linklock1/", "lockfile")
+    elif config == "linklock2":
+        return LinkLock2(dir + "/./linklock2/", "lockfile")
+    elif config == "openlock":
+        return OpenLock(dir + "/./openlock/", "lockfile")
+    else:
+        raise RuntimeError("Unknown Lock Type")
+
